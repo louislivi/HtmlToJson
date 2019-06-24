1c4425f7c09f05e91e6f18f0312c602f1309916d
louislivi <574747417@qq.com>
2019-06-24 16:55:16 +0800

    Fix text node offset
    Fix text replace error

--- HtmlToJson.java
+++ HtmlToJson.java
@@ -2,11 +2,7 @@ package com.louislivi.htmltojson;
 
 import com.alibaba.fastjson.JSONObject;
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Attribute;
-import org.jsoup.nodes.Attributes;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import org.jsoup.nodes.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -31,26 +27,37 @@ public class HtmlToJson {
         List<Map<String, Object>> nodes = new ArrayList<>();
         html = HtmlToJson.htmlParse(html);
         Document document = Jsoup.parse(html);
-        Elements elements = document.getElementsByTag("body").get(0).children();
-        return JSONObject.toJSONString(html2json(nodes, elements));
+        List<Node> nodeList = document.getElementsByTag("body").get(0).childNodes();
+        return JSONObject.toJSONString(html2json(nodes, nodeList));
     }
 
     /**
      * html转json
      *
      * @param nodes
-     * @param elements
+     * @param nodeList
      * @return
      */
-    private static List<Map<String, Object>> html2json(List<Map<String, Object>> nodes, Elements elements) {
-        for (Element element : elements) {
-            String nodeName = HtmlToJson.replaceNodeName(element.nodeName());
+    private static List<Map<String, Object>> html2json(List<Map<String, Object>> nodes, List<Node> nodeList) {
+        for (Node node : nodeList) {
+            String nodeName = HtmlToJson.replaceNodeName(node.nodeName());
             if (!HtmlToJson.nodeFilter(nodeName)) {
                 continue;
             }
             Map<String, Object> map = new HashMap<>(4);
-            map.put("name", nodeName);
-            Attributes attributes = element.attributes();
+
+            //标签名
+            if ("#text".equals(nodeName)) {
+                //标签文本
+                map.put("type", "text");
+                System.out.println(node.toString());
+                map.put("text", "".equals(node.toString()) ? " " : node.toString().replace("(|M-Y|)", "&#"));
+            } else {
+                map.put("name", nodeName);
+            }
+
+            //处理标签属性
+            Attributes attributes = node.attributes();
             Map<String, String> attrs = new HashMap<>(attributes.size());
             for (Attribute attribute : attributes) {
                 if (HtmlToJson.attrFilter(attribute.getKey())) {
@@ -63,14 +70,9 @@ public class HtmlToJson {
             if (!attrs.isEmpty()) {
                 map.put("attrs", attrs);
             }
+
             List<Map<String, Object>> childrens = new ArrayList<>();
-            if (!"".equals(element.ownText())) {
-                Map<String, Object> children = new HashMap<>(4);
-                children.put("type", "text");
-                children.put("text", "".equals(element.ownText()) ? " " : element.ownText().replaceAll("&m#", "&#"));
-                childrens.add(0, children);
-            }
-            HtmlToJson.html2json(childrens, element.children());
+            HtmlToJson.html2json(childrens, node.childNodes());
             if (!childrens.isEmpty()) {
                 map.put("children", childrens);
             }
@@ -86,7 +88,7 @@ public class HtmlToJson {
      * @return
      */
     private static String replaceNodeName(String nodeName) {
-        return nodeName.replaceAll("section", "div");
+        return nodeName.replace("section", "div");
     }
 
     /**
@@ -96,7 +98,7 @@ public class HtmlToJson {
      * @return
      */
     private static Boolean nodeFilter(String nodeName) {
-        String pattern = "p|a|abbr|b|blockquote|br|code|col|colgroup|dd|del|div|dl|dt|em|fieldset|h1|h2|h3|h4|h5|h6|hr|i|img|ins|label|legend|li|olp|q|span|strong|sub|sup|table|tbody|td|tfoot|th|thead|tr|ul";
+        String pattern = "p|a|abbr|b|blockquote|br|code|col|colgroup|dd|del|div|dl|dt|em|fieldset|h1|h2|h3|h4|h5|h6|hr|i|img|ins|label|legend|li|olp|q|span|strong|sub|sup|table|tbody|td|tfoot|th|thead|tr|ul|#text";
         return Pattern.matches(pattern, nodeName);
     }
 
@@ -118,21 +120,21 @@ public class HtmlToJson {
      * @return
      */
     private static String htmlParse(String html) {
-        return html.replaceAll("&nbsp", "&m#160")
-                .replaceAll("&lt", "&m#60")
-                .replaceAll("&gt", "&m#62")
-                .replaceAll("&amp", "&m#38")
-                .replaceAll("&quot", "&m#34")
-                .replaceAll("&apos", "&m#39")
-                .replaceAll("&cent", "&m#162")
-                .replaceAll("&pound", "&m#163")
-                .replaceAll("&yen", "&m#165")
-                .replaceAll("&euro", "&m#8364")
-                .replaceAll("&sect", "&m#167")
-                .replaceAll("&copy", "&m#169")
-                .replaceAll("&reg", "&m#174")
-                .replaceAll("&trade", "&m#8482")
-                .replaceAll("&times", "&m#215")
-                .replaceAll("&divide", "&m#247");
+        return html.replace("&nbsp", "(|M-Y|)160")
+                .replace("&lt", "(|M-Y|)60")
+                .replace("&gt", "(|M-Y|)62")
+                .replace("&amp", "(|M-Y|)38")
+                .replace("&quot", "(|M-Y|)34")
+                .replace("&apos", "(|M-Y|)39")
+                .replace("&cent", "(|M-Y|)162")
+                .replace("&pound", "(|M-Y|)163")
+                .replace("&yen", "(|M-Y|)165")
+                .replace("&euro", "(|M-Y|)8364")
+                .replace("&sect", "(|M-Y|)167")
+                .replace("&copy", "(|M-Y|)169")
+                .replace("&reg", "(|M-Y|)174")
+                .replace("&trade", "(|M-Y|)8482")
+                .replace("&times", "(|M-Y|)215")
+                .replace("&divide", "(|M-Y|)247");
     }
 }
